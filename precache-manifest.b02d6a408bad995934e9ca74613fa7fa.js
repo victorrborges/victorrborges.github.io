befc7d5feeeca28207f4789f323c0e7763172ba6
victorrborges <victor.araujo@ccc.ufcg.edu.br>
2019-08-29 22:01:27 -0300

    Updates

---
+++ precache-manifest.b02d6a408bad995934e9ca74613fa7fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "085bd11a0aa73f4dbf01f4cf5b3b422e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5727cad319bd5e379369",
+    "url": "/static/css/main.0e1e2934.chunk.css"
+  },
+  {
+    "revision": "21d374903c7e5529b265",
+    "url": "/static/js/2.4b1ba94f.chunk.js"
+  },
+  {
+    "revision": "5727cad319bd5e379369",
+    "url": "/static/js/main.07fda9b4.chunk.js"
+  },
+  {
+    "revision": "c49018507a564c0ada45",
+    "url": "/static/js/runtime~main.b908f969.js"
+  },
+  {
+    "revision": "f66ab8591dd416e492cfe46615e0cc5e",
+    "url": "/static/media/portfolio.f66ab859.png"
+  }
+]);
